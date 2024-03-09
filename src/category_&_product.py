274f6f40648f43d792302c78c06d3791049cf5b6
konov <konovalova_ekaterina_a@mail.ru>
2024-03-09 19:27:26 +0300

    add def__init__ for both classes

--- src/category_&_product.py
+++ src/category_&_product.py
@@ -1,5 +1,22 @@
 class Category:
-    pass
+    name = str
+    descriptions = str
+    goods = list
+
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.descriptions = description
+        self.goods = goods
+
 
 class Product:
-    pass
+    name = str
+    description = str
+    price = float
+    quantity_in_stock = int
+
+    def __init__(self, name, description, price, quantity_in_stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
