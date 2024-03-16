f0cb57838c57e169f613d4d3c00422e161e09236
konov <konovalova_ekaterina_a@mail.ru>
2024-03-16 17:33:20 +0300

    completed version

--- src/category_product.py
+++ src/category_product.py
@@ -2,21 +2,22 @@ class Category:
     """Класс для представления категории продукта"""
     name = str
     descriptions = str
-    goods = list
+    goods_list = list
 
     # Артибут класса, подсчет количества категорий
     number_of_categories = 0
     # Атрибут класса, подсчет количества уникальных товаров
     number_of_uniq_goods = 0
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, goods_list):
         """Метод для инициализации экземпляра класса"""
         self.name = name
         self.description = description
+        self.__goods_list = goods_list
         self.__goods = {}
 
         Category.number_of_categories += 1
-        Category.number_of_uniq_goods += len(set())
+        Category.number_of_uniq_goods += len(set(self.__goods_list))
 
     def add_product(self, *prods):
         """Добавление нового товара в список. Если такой товар уже есть в self.__goods, выбирается  наибольшее значение
@@ -83,15 +84,18 @@ class Product:
                 answer = input(f'Новая цена ниже. Меняем цену? y, n: ')
                 if answer == 'y':
                     self.__price = new_price
+                elif answer != 'n' and answer != 'y':
+                    print('Цена останется прежней')
                 else:
                     return
             self.__price = new_price
 
 
 category_1 = Category('Смартфоны', 'Смартфоны, как средство не только коммуникации, но и получение'
-                                   ' дополнительных функций для удобства жизни')
+                                   ' дополнительных функций для удобства жизни', ['Samsung Galaxy C23 Ultra',
+                                                                                  'Iphone 15', 'Xiaomi Redmi Note 11'])
 category_2 = Category('Телевизоры', 'Современный телевизор, который позволяет наслаждаться просмотром,'
-                                    ' станет вашим другом и помощником')
+                                    ' станет вашим другом и помощником', ['55" QLED 4K'])
 product_1 = Product('Samsung Galaxy C23 Ultra', '256GB, Серый цвет, 200MP камера', 180000.0, 5)
 product_2 = Product('Iphone 15', '512GB, Gray space', 210000.0, 8)
 product_3 = Product('Xiaomi Redmi Note 11', '1024GB, Синий', 31000.0, 14)
@@ -109,3 +113,6 @@ print(category_1.products)
 
 product_1.price = float(input('Введите цену: '))
 print(category_1.products)
+
+print('Количество продуктов: ', Category.number_of_uniq_goods)
+print('Количество категорий: ', Category.number_of_categories)
